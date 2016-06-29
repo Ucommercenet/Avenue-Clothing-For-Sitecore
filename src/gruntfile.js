c2de90953a16cde5407cfa1fdfa61efb19a899f6
Lasse Eskildsen <les@ucommerce.net>
2016-06-29 15:09:04 +0200

    Web site setup

---
+++ src/gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        src: {
+            b2cRoot: "AvenueClothing.B2C/",
+            directories: ["bin/*.dll", "app_config/include/*.config", "views/**"],
+            destination: "c:/inetpub/scdev/website"
+        } ,
+        
+        copy: {
+            b2c: {
+                files: [                    
+                    { expand: true, cwd: "<%= src.b2cRoot %>", src: "<%= src.directories %>", dest: "<%= src.destination %>", nonull: true }
+           
+                ]
+            }
+        },
+        
+        watch: {
+            b2c: {
+                files: "<%= src.b2cRoot %>",
+                tasks: ["copy"],
+                livereload: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+};
