e3c5e9c386024aa395c881aa3e87cd1c1fa5042a
Mads Due <devnull@localhost>
2016-09-23 12:54:34 +0200

    Serialization now works with unicorn and rainbow.

---
+++ src/gulp-config.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+	var instanceRoot = "C:\\inetpub\\SC8\\Website";
+	var config = {
+		websiteRoot: instanceRoot,
+		sitecoreLibraries: instanceRoot + "\\bin",
+		licensePath: instanceRoot + "\\App_Data\\license.xml",
+		solutionName: "AvenueClothing",
+		buildConfiguration: "Debug",
+		runCleanBuilds: true
+	};
+	return config;
+}
