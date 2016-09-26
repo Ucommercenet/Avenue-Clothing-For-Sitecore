3c5c34ac950c1113392a58cbf51afd09d807c3ba
Mads Due <devnull@localhost>
2016-09-26 15:03:09 +0200

    Forgot the sitecore items in last commit.

---
+++ src/AvenueClothing.B2C/Controllers/HeaderController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AvenueClothing.Project.Website.Controllers
+{
+    public class HeaderController : Controller
+    {
+		public ActionResult Header()
+		{
+			return View("/views/Avenue Clothing/header.cshtml");
+		}
+    }
+}
--- src/AvenueClothing.Feature.Catalog.Module/layouts/commerce/catalog/Category Navigation.ascx
+++
@@ -1,5 +0,0 @@
-﻿<%@ Control Language="c#" AutoEventWireup="true" TargetSchema="http://schemas.microsoft.com/intellisense/ie5" %>
-
-<h1>
-	Hello world from Category Navigation
-</h1>
