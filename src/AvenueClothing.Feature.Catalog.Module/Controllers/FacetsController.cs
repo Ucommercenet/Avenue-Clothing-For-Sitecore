5c4f792f3a121049718486aed16c94749ddf0c38
Mads Due <devnull@localhost>
2016-10-04 14:19:19 +0200

    Create a facets module and update the image of how the category definition template should look like.

---
+++ src/AvenueClothing.Feature.Catalog.Module/Controllers/FacetsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using AvenueClothing.Feature.Catalog.Module.ViewModels;
+using UCommerce.Api;
+using UCommerce.Content;
+using UCommerce.Extensions;
+using UCommerce.Infrastructure;
+using UCommerce.Runtime;
+using UCommerce.Search.Facets;
+using UCommerce.Search;
+
+namespace AvenueClothing.Feature.Catalog.Module.Controllers
+{
+	public class FacetsController : Controller
+	{
+		public ActionResult Facets()
+		{
+
+
+			return View("/views/Facets.cshtml");
+		}
+
+	}
+
+}
