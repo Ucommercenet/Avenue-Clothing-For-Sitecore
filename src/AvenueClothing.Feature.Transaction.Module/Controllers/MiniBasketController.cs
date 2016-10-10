1bdcd59a84b2318c0de088a1b70dd03f6e2da27a
Thomas Arvidsen <devnull@localhost>
2016-10-10 14:26:18 +0200

    Added controller and view.

--- src/AvenueClothing.Feature.Transaction.Module/Controllers/MiniBasketController.cs
+++ src/AvenueClothing.Feature.Transaction.Module/Controllers/MiniBasketController.cs
@@ -1,8 +1,29 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using AvenueClothing.Feature.Transaction.Module.ViewModels;
+using UCommerce;
+using UCommerce.Api;
 
 namespace AvenueClothing.Feature.Transaction.Module.Controllers
 {
 	public class MiniBasketController : Controller
 	{
+		public ActionResult MiniBasket()
+		{
+			var miniBasketViewModel = new MiniBasketViewModel { IsEmpty = true };
+
+			if (!TransactionLibrary.HasBasket())
+			{
+				return View(miniBasketViewModel);
+			}
+
+			var purchaseOrder = TransactionLibrary.GetBasket(false).PurchaseOrder;
+
+			miniBasketViewModel.NumberOfItems = purchaseOrder.OrderLines.Sum(x => x.Quantity);
+			miniBasketViewModel.IsEmpty = miniBasketViewModel.NumberOfItems == 0;
+			miniBasketViewModel.Total = purchaseOrder.OrderTotal.HasValue ? new Money(purchaseOrder.OrderTotal.Value, purchaseOrder.BillingCurrency) : new Money(0, purchaseOrder.BillingCurrency);
+
+			return View(miniBasketViewModel);
+		}
 	}
 }
---
+++ src/AvenueClothing.Feature.Transaction.Module/ViewModels/MiniBasketViewModel.cs
@@ -0,0 +1,11 @@
+﻿using UCommerce;
+
+namespace AvenueClothing.Feature.Transaction.Module.ViewModels
+{
+	public class MiniBasketViewModel
+	{
+		public int NumberOfItems { get; set; }
+		public Money Total { get; set; }
+		public bool IsEmpty { get; set; }
+	}
+}
