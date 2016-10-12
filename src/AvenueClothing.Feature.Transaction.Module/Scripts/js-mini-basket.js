40902f3cb0a96141648aee9059ba0ec70e477f7f
Thomas Arvidsen <devnull@localhost>
2016-10-12 14:44:24 +0200

    update to match the new way of commication between components.

--- src/AvenueClothing.Feature.Transaction.Module/Scripts/js-mini-basket.js
+++ src/AvenueClothing.Feature.Transaction.Module/Scripts/js-mini-basket.js
@@ -1,52 +1,46 @@
 ﻿var MiniBasket = (function () {
 
 	// declared with `var`, must be "private"
-	var privateMethod = function () { };
+	var classSelector = ".js-mini-basket";
+
+	var basketChanged = function (event, data) {
+		var $miniBasket = $(this);
+
+		var emptySelector = $miniBasket.data("mini-basket-empty-selector");
+		var notEmptySelector = $miniBasket.data("basket-not-empty-selector");
+		var numberOfItemsSelector = $miniBasket.data("mini-basket-number-of-items-selector");
+		var totalSelector = $miniBasket.data("mini-basket-total-selector");
+
+		var refreshUrl = $(this).data("refresh-url");
+		$.ajax({
+			type: "GET",
+			url: refreshUrl,
+			dataType: "json"
+		}
+			.done(function (data) {
+				if (data.IsEmpty) {
+					$miniBasket.find(notEmptySelector).addClass("hidden-xl-down");
+					$miniBasket.find(emptySelector).removeClass("hidden-xl-down");
+				} else {
+					$miniBasket.find(numberOfItemsSelector).text(data.NumberOfItems);
+					$miniBasket.find(totalSelector).text(data.Total);
+
+					$miniBasket.find(notEmptySelector).removeClass("hidden-xl-down");
+					$miniBasket.find(emptySelector).addClass("hidden-xl-down");
+				}
+			})
+			.fail(function () {
+				alert("Whoops...");
+			})
+			.always(function () {
+				//No-op
+			}));
+	}
+
 
 	var publicScope = {
 		init: function (rootSelector) {
-			//$(rootSelector).find(".js-mini-basket")
-		},
-		basketChanged: function (rootSelector) {
-			alert('TODO: Update mini basket');
-
-			$(rootSelector).find(".js-mini-basket")
-			   .each(function () {
-			   	var $miniBasket = $(this);
-			   	var validateProductExistsUrl = $(this).data("validate-product-exists-url");
-			   	$.ajax({
-			   		type: "GET",
-			   		url: validateProductExistsUrl,
-			   		data:
-					{
-						ProductSku: product.productSku,
-						VariantSku: product.variantSku
-					},
-			   		dataType: "json"
-			   	}
-				.done(function (data) {
-					if (data.IsEmpty) {
-						$miniBasket.find("js-mini-basket-not-empty").addClass("hidden-*");
-						$miniBasket.find("js-mini-basket-empty").removeClass("hidden-*");
-					} else {
-						$miniBasket.find(".js-mini-basket-number-of-items").text(data.NumberOfItems);
-						$miniBasket.find(".js-mini-basket-total").text(data.Total);
-
-						$miniBasket.find("js-mini-basket-not-empty").removeClass("hidden-*");
-						$miniBasket.find("js-mini-basket-empty").addClass("hidden-*");
-					}
-				})
-				.fail(function () {
-					alert("Whoops...");
-				})
-				.always(function () {
-					//No-op
-				}));
-			   });
-
-			
-
-
+			$(rootSelector).find(".js-mini-basket").on("basket-changed", basketChanged);
 		}
 	};
 
