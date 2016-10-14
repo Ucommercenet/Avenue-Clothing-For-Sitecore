3f6473a8e32ede4555d5e766e4df97f575e3b3c9
alp <alp@ucommerce.net>
2016-10-14 10:00:56 +0200

    Breadcrumb does not render anything because of the tree structure or lack of thereof. Changing to our breadcrumbs instead.

--- src/AvenueClothing.Feature.General.Module/Controllers/BreadcrumbController.cs
+++ src/AvenueClothing.Feature.General.Module/Controllers/BreadcrumbController.cs
@@ -1,14 +1,62 @@
-﻿using System;
+﻿using AvenueClothing.Feature.General.Module.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-
+using UCommerce.EntitiesV2;
+using UCommerce.Runtime;
 
 namespace AvenueClothing.Project.Website.Controllers
 {
     public class BreadcrumbController: Controller
     {
+        public ActionResult Index()
+        {
+            IList<BreadcrumbViewModel> breadcrumbs = new List<BreadcrumbViewModel>();
+            Category lastCategory = null;
+            Product product = SiteContext.Current.CatalogContext.CurrentProduct;
 
+            foreach (var category in SiteContext.Current.CatalogContext.CurrentCategories)
+            {
+                var breadcrumb = new BreadcrumbViewModel()
+                {
+                    BreadcrumbName = category.DisplayName(),
+                    BreadcrumbUrl = CatalogLibrary.GetNiceUrlForCategory(category)
+                };
+                lastCategory = category;
+                breadcrumbs.Add(breadcrumb);
+            }
+
+            if (product != null)
+            {
+                var breadcrumb = new BreadcrumbsViewModel()
+                {
+                    BreadcrumbName = product.DisplayName(),
+                    BreadcrumbUrl = CatalogLibrary.GetNiceUrlForProduct(product, lastCategory)
+                };
+                breadcrumbs.Add(breadcrumb);
+            }
+
+            if (product == null && lastCategory == null)
+            {
+                var currentNode = UmbracoContext.Current.PublishedContentRequest.PublishedContent;
+                foreach (var level in currentNode.Ancestors().Where("Visible"))
+                {
+                    var breadcrumb = new BreadcrumbsViewModel()
+                    {
+                        BreadcrumbName = level.Name,
+                        BreadcrumbUrl = level.Url
+                    };
+                    breadcrumbs.Add(breadcrumb);
+                }
+                var currentBreadcrumb = new BreadcrumbsViewModel()
+                {
+                    BreadcrumbName = currentNode.Name,
+                    BreadcrumbUrl = currentNode.Url
+                };
+                breadcrumbs.Add(currentBreadcrumb);
+            }
+
+            return View("/Views/PartialView/Breadcrumbs.cshtml", breadcrumbs);
+        }
     }
 }
