d8248f8c14e8cdad06cf1d32c0e2d79cca5a5c94
Mads Due <devnull@localhost>
2016-10-18 12:13:01 +0200

    js to deteckt when user is scroll to the bottom of category page.

---
+++ src/AvenueClothing.Feature.Catalog.Module/Scripts/js-infinite-scrolling.js
@@ -0,0 +1,16 @@
+﻿var InfiniteScrolling = (function () {
+	var inScope = false;
+	$(window).scroll(function () {
+		if ($(window).scrollTop() + $(window).height() > $(document).height() - 200) {
+			if (!inScope) {
+				if ($(".category .product-list").length > 0) {
+					var productsToSkip = $(".category .product-list .product").length;
+					console.log("Number of already loaded products: " + productsToSkip);
+				}
+				inScope = true;
+			}
+		} else {
+			inScope = false;
+		}
+	});
+})();
