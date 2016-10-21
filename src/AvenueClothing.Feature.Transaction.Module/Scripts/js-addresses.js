8266b8da20d4fa7395bc1aaee3f2ce3fd972cd00
Mads Due <devnull@localhost>
2016-10-21 10:50:45 +0200

    Addresses page.

---
+++ src/AvenueClothing.Feature.Transaction.Module/Scripts/js-addresses.js
@@ -0,0 +1,35 @@
+﻿var Addresses = (function () {
+	var classSelector = ".js-addresses";
+
+
+	var toggleShippingAddress = function () {
+		var shippingAddressElement = $(classSelector + ".Shipping");
+		if (shippingAddressElement == null) return;
+
+		if (shippingAddressElement.is(":visible")) {
+			shippingAddressElement.hide();
+		} else {
+			shippingAddressElement.show();
+		}
+	};
+
+	if ($(classSelector)) {
+		var billingAddressContentElement = $(classSelector + ".Billing" + " .well");
+		if (billingAddressContentElement == null) return;
+
+		var checkbox = document.createElement('input');
+		checkbox.type = "checkbox";
+		checkbox.addEventListener('click', function () {
+			toggleShippingAddress(); return false;
+		});
+
+		var label = document.createElement("label");
+		label.innerHTML = "Shipping address same as billing";
+
+		billingAddressContentElement[0].appendChild(checkbox);
+		billingAddressContentElement[0].appendChild(label);
+	}
+
+	
+})();
+
---
+++ src/AvenueClothing.Feature.Transaction.Module/ViewModels/AddressDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace AvenueClothing.Feature.Transaction.Module.ViewModels
+{
+	public class AddressDetailsViewModel
+	{
+		public AddressDetailsViewModel() 
+        {
+			ShippingAddress = new AddressViewModel();
+			BillingAddress = new AddressViewModel();
+			AvailableCountries = new List<SelectListItem>();
+            IsShippingAddressDifferent = false;
+        }
+		public AddressViewModel ShippingAddress { get; set; }
+
+		public AddressViewModel BillingAddress { get; set; }
+
+        public bool IsShippingAddressDifferent { get; set; }
+
+		public IList<SelectListItem> AvailableCountries { get; set; }
+	}
+}
