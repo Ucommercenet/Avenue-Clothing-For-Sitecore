fb2c5e12e79d90934a9b0151669999ae4522fcd9
Thomas Arvidsen <devnull@localhost>
2016-11-01 13:02:27 +0100

    started on the voucher module.

---
+++ src/AvenueClothing.Feature.Transaction.Module/Controllers/VoucherController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using Sitecore.Mvc.Controllers;
+using UCommerce.Marketing;
+using UCommerce.Transactions;
+
+namespace AvenueClothing.Feature.Transaction.Module.Controllers
+{
+	public class VoucherController : SitecoreController
+	{
+		private readonly MarketingLibraryInternal _marketingLibraryInternal;
+		private readonly TransactionLibraryInternal _transactionLibraryInternal;
+
+		public VoucherController(MarketingLibraryInternal marketingLibraryInternal, TransactionLibraryInternal transactionLibraryInternal)
+		{
+			_marketingLibraryInternal = marketingLibraryInternal;
+			_transactionLibraryInternal = transactionLibraryInternal;
+		}
+
+		public ActionResult Rendering()
+		{
+			return View();
+		}
+
+		public ActionResult AddVoucher(string voucher)
+		{
+			_marketingLibraryInternal.AddVoucher(voucher);
+			_transactionLibraryInternal.ExecuteBasketPipeline();
+
+			return Json(new { });
+
+		}
+	}
+}
