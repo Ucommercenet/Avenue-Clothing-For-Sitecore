0f2c66d73de2cc85d6171f4d9fbecd217d3a1115
Mads Due <devnull@localhost>
2016-11-08 14:37:54 +0100

    Minor clean up.

--- src/AvenueClothing.Feature.General.Module/ViewModels/BreadcrumbViewModel.cs
+++ src/AvenueClothing.Feature.General.Module/ViewModels/BreadcrumbViewModel.cs
@@ -4,12 +4,7 @@ namespace AvenueClothing.Feature.General.Module.ViewModels
 {
     public class BreadcrumbViewModel : CustomItem   
     {
-
-        public BreadcrumbViewModel(Item innerItem) : base(innerItem)
-        {
-
-        }
-
+		public BreadcrumbViewModel(Item innerItem) : base(innerItem){ }
         public string BreadcrumbName{ get; set; }
         public bool IsActive{ get; set; }
         public string BreadcrumbUrl{ get; set; }
