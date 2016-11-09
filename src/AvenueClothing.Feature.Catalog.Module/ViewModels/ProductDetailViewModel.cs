fb06ae3cc814ba2f9e8f4c2858c6ea097fedb3ca
Mads Due <devnull@localhost>
2016-11-09 08:59:16 +0100

    Refactored to be a controll rendering.

---
+++ src/AvenueClothing.Feature.Catalog.Module/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Web;
+
+namespace AvenueClothing.Feature.Catalog.Module.ViewModels
+{
+	public class ProductDetailViewModel
+	{
+		public HtmlString LongDescription { get; set; }
+	}
+}
