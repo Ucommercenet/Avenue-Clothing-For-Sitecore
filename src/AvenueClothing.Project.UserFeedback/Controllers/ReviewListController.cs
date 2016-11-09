3271a89f2219febd50f79b017689785072f10c0a
alp <alp@ucommerce.net>
2016-11-09 13:51:49 +0100

    New Reviews module

---
+++ src/AvenueClothing.Project.UserFeedback/Controllers/ReviewListController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using AvenueClothing.Feature.Catalog.Module.ViewModels;
+using AvenueClothing.Foundation.MvcExtensionsModule;
+using UCommerce.Runtime;
+
+namespace AvenueClothing.Feature.Catalog.Module.Controllers
+{
+	public class ReviewListController : BaseController
+    {
+	    private readonly ICatalogContext _catalogContext;
+
+	    public ReviewListController(ICatalogContext catalogContext)
+	    {
+		    _catalogContext = catalogContext;
+	    }
+
+	    public ActionResult Rendering()
+	    {
+	        var viewModel = new ReviewListRenderingViewModel();
+
+            var currentProduct = _catalogContext.CurrentProduct;
+
+            viewModel.Reviews = currentProduct.ProductReviews.Select(review => new ReviewListRenderingViewModel.Review
+            {
+	            Name = review.Customer.FirstName + " " + review.Customer.LastName,
+	            Email = review.Customer.EmailAddress,
+	            Title = review.ReviewHeadline,
+	            CreatedOn = review.CreatedOn,
+	            Comments = review.ReviewText,
+	            Rating = review.Rating
+	        }).ToList();
+
+            return View(viewModel);
+        }
+    }
+}
--- src/gulp-config.js
+++ src/gulp-config.js
@@ -2,7 +2,7 @@ module.exports = function () {
 	var instanceRoot = "C:\\inetpub\\sc8dev\\Website";
 	var config = {
 		websiteRoot: instanceRoot,
-		websiteUrl: "http://sc8accelerator",
+		websiteUrl: "http://sc8dev",
 		sitecoreLibraries: instanceRoot + "\\bin",
 		licensePath: instanceRoot + "\\App_Data\\license.xml",
 		solutionName: "AvenueClothing",
