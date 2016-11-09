4a2b01dcdfd7e9795dc4d4cf93685272a3f71f5d
Mads Due <devnull@localhost>
2016-11-09 09:14:39 +0100

    Refactored to be a control rendering.

---
+++ src/AvenueClothing.Feature.Catalog.Module/ViewModels/ProductPrimaryImageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Web;
+
+namespace AvenueClothing.Feature.Catalog.Module.ViewModels
+{
+	public class ProductPrimaryImageViewModel
+	{
+		public HtmlString PrimaryImage { get; set; }
+	}
+}
