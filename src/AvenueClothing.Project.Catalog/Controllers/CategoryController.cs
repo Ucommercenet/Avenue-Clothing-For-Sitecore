5aedc569fce9b0cdc4268e44a802860a7480334a
alp <alp@ucommerce.net>
2016-11-30 17:37:19 +0100

    Removed deleted files from Solution

--- src/AvenueClothing.Project.Catalog/Controllers/CategoryController.cs
+++ src/AvenueClothing.Project.Catalog/Controllers/CategoryController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using AvenueClothing.Project.Catalog.Extensions;
 using AvenueClothing.Project.Catalog.ViewModels;
 using AvenueClothing.Foundation.MvcExtensions;
 using AvenueClothing.Project.Catalog.Services;
