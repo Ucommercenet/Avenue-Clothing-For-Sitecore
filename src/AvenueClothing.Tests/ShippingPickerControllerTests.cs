d3784ca8128c6f93f36506d349ddd324ae56ec5c
alp <alp@ucommerce.net>
2016-12-07 17:14:56 +0100

    Testing WIP for Shipping Picker

--- src/AvenueClothing.Tests/ShippingPickerControllerTests.cs
+++ src/AvenueClothing.Tests/ShippingPickerControllerTests.cs
@@ -7,6 +7,9 @@ using System.Web.Mvc;
 using AvenueClothing.Project.Transaction.Controllers;
 using AvenueClothing.Project.Transaction.ViewModels;
 using NSubstitute;
+using Sitecore.ApplicationCenter.Applications;
+using Sitecore.Shell.Framework.Commands.Masters;
+using UCommerce;
 using UCommerce.EntitiesV2;
 using UCommerce.Transactions;
 using Xunit;
@@ -29,22 +32,29 @@ namespace AvenueClothing.Tests
         {
             //Arrange
             var purchaseOrder = new PurchaseOrder();
-            _transactionLibraryInternal.GetBasket(false).Returns(new Basket(new PurchaseOrder {});
+           var orderaddress = new OrderAddress {AddressName = Constants.DefaultShipmentAddressName, Country = new Country()};
+            purchaseOrder.OrderAddresses.Add(orderaddress);
+            purchaseOrder.OrderStatus = new OrderStatus();
+            _transactionLibraryInternal.GetBasket(false).Returns(new Basket(purchaseOrder));
+
             //Act
             var result = _controller.Rendering();
+
             //Assert
             var viewResult = result as ViewResult;
             var model = viewResult != null ? viewResult.Model as ShippingPickerViewModel : null;
             Assert.NotNull(viewResult);
             Assert.NotNull(model);
-            Assert.NotEmpty(model.AvailableShippingMethods);
-            Assert.NotNull(model.SelectedShippingMethodId);
-            Assert.NotNull(model.ShippingCountry);
+            Assert.Equal(-1, model.SelectedShippingMethodId);
         }
 
         [Fact]
         public void Save_Shipping_Method_Should_Execute_Basket_Pipeline_And_Return_Json()
         {
+            var shipment= new CreateShipmentViewModel();
+
+            _transactionLibraryInternal.CreateShipment(shipment.ShippingMethodId, Constants.DefaultShipmentAddressName, true);
+            //_transactionLibraryInternal.ExecuteBasketPipeline();
 
         }
     }
