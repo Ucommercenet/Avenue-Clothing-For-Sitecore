549f4653ed865f1e78e7de7cfce4001ba3dd684f
alp <alp@ucommerce.net>
2016-12-07 12:00:48 +0100

    Added some properties and some renderings tot the new page.

--- src/AvenueClothing.Project.Transaction/ViewModels/QuantityPickerRenderingViewModel.cs
+++ src/AvenueClothing.Project.Transaction/ViewModels/QuantityPickerRenderingViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace AvenueClothing.Project.Transaction.ViewModels
+﻿using System.Web;
+
+namespace AvenueClothing.Project.Transaction.ViewModels
 {
     public class QuantityPickerRenderingViewModel
     {
+        public HtmlString QuantityLabel { get; set; }
         public string ProductSku { get; set; }
         public int MaxNumberOfItems { get; set; }
     }
