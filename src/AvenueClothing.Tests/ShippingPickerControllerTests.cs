9049d5b2b1bf8348e184a08c6425e97c7d9f5ca8
alp <alp@ucommerce.net>
2016-12-09 11:01:44 +0100

    Done Shipping Picker Testing.

--- src/AvenueClothing.Tests/ShippingPickerControllerTests.cs
+++ src/AvenueClothing.Tests/ShippingPickerControllerTests.cs
@@ -32,7 +32,7 @@ namespace AvenueClothing.Tests
         {
             //Arrange
             var purchaseOrder = new PurchaseOrder();
-           var orderaddress = new OrderAddress {AddressName = Constants.DefaultShipmentAddressName, Country = new Country()};
+            var orderaddress = new OrderAddress { AddressName = Constants.DefaultShipmentAddressName, Country = new Country() };
             purchaseOrder.OrderAddresses.Add(orderaddress);
             purchaseOrder.OrderStatus = new OrderStatus();
             _transactionLibraryInternal.GetBasket(false).Returns(new Basket(purchaseOrder));
@@ -49,13 +49,22 @@ namespace AvenueClothing.Tests
         }
 
         [Fact]
-        public void Save_Shipping_Method_Should_Execute_Basket_Pipeline_And_Return_Json()
+        public void Save_Shipping_Method_Should_Execute_Basket_Pipeline_And_Create_Shippiment()
         {
-            var shipment= new CreateShipmentViewModel();
+            //Arrange
+            var shipping = new ShippingPickerViewModel();
 
-            _transactionLibraryInternal.CreateShipment(shipment.ShippingMethodId, Constants.DefaultShipmentAddressName, true);
-            //_transactionLibraryInternal.ExecuteBasketPipeline();
+            //Act
+            var result = _controller.CreateShipment(shipping);
+            //var jsonResult = result as JsonResult;
+       
+            //Assert
+            Assert.NotNull(shipping.SelectedShippingMethodId);
+            //Assert.NotNull(jsonResult);
 
-        }
+            _transactionLibraryInternal.Received().ExecuteBasketPipeline();
+            _transactionLibraryInternal.Received().CreateShipment(shipping.SelectedShippingMethodId, Constants.DefaultShipmentAddressName, true);
+
+      }
     }
 }
