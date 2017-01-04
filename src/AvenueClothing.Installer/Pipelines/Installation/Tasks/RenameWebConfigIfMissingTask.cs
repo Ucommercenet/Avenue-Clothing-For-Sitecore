e78905bb9ef15aa5e02721b47592ed07bd8158c4
MSK <devnull@localhost>
2017-01-04 11:37:24 +0100

    Some versions of Sitecore does not ship a web.config under views folder. Add bare minimum web.config if doesn't exists.

---
+++ src/AvenueClothing.Installer/Pipelines/Installation/Tasks/RenameWebConfigIfMissingTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Hosting;
+using UCommerce.Pipelines;
+
+namespace AvenueClothing.Installer.Pipelines.Installation.Tasks
+{
+    public class RenameWebConfigIfMissingTask : IPipelineTask<InstallationPipelineArgs>
+    {
+        public RenameWebConfigIfMissingTask()
+        {
+            
+        }
+
+        public PipelineExecutionResult Execute(InstallationPipelineArgs subject)
+        {
+            var webConfig = new FileInfo(HostingEnvironment.MapPath("~/Views/Web.config"));
+
+            if (webConfig.Exists)
+            {
+                return PipelineExecutionResult.Success;
+            }
+
+            new UCommerce.Installer.FileMover(
+                new FileInfo(HostingEnvironment.MapPath("~/Views/Web.config.default")),
+                new FileInfo(HostingEnvironment.MapPath("~/Views/Web.config"))).Move(false, (Exception ex) => { throw ex; });
+
+            return PipelineExecutionResult.Success;
+        }
+    }
+}
