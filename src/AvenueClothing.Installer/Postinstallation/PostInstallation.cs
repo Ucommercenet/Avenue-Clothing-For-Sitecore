e6293e03538f5729b1e225e3b21c1895bc5122b2
MSK <devnull@localhost>
2017-01-04 11:35:15 +0100

    Empty field was never used.

--- src/AvenueClothing.Installer/Postinstallation/PostInstallation.cs
+++ src/AvenueClothing.Installer/Postinstallation/PostInstallation.cs
@@ -6,13 +6,6 @@ namespace AvenueClothing.Installer.Postinstallation
 {
     public class PostInstallation : IPostStep
     {
-        private readonly IList<IPostStep> _composite;
-
-        public PostInstallation()
-        {
-
-        }
-
         public void Run(ITaskOutput output, NameValueCollection metaData)
         {
             
