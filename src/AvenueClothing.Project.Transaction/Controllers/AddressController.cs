17aecc09ea6270144ba3d3648614321c70d5cc1c
Morten Lyhr <morten.lyhr@ucommerce.net>
2017-01-09 11:42:13 +0100

    Remove Sitecore analytics contract tracking

--- src/AvenueClothing.Project.Transaction/Controllers/AddressController.cs
+++ src/AvenueClothing.Project.Transaction/Controllers/AddressController.cs
@@ -5,9 +5,6 @@ using UCommerce.EntitiesV2;
 using UCommerce.Transactions;
 using AvenueClothing.Foundation.MvcExtensions;
 using AvenueClothing.Project.Transaction.ViewModels;
-using Sitecore.Analytics;
-using Sitecore.Commerce.Entities.Customers;
-using Sitecore.Commerce.Services.Customers;
 using Constants = UCommerce.Constants;
 
 namespace AvenueClothing.Project.Transaction.Controllers
@@ -95,26 +92,7 @@ namespace AvenueClothing.Project.Transaction.Controllers
                 EditBillingInformation(addressRendering.BillingAddress);
                 EditShippingInformation(addressRendering.BillingAddress);
             }
-
-            var email = addressRendering.BillingAddress.EmailAddress;
-            if (!string.IsNullOrEmpty(email))
-            {
-                if (email.Contains("@"))
-                {
-                    Tracker.Current.Session.Identify(email);
-
-                    var emails = Tracker.Current.Contact.GetFacet<Sitecore.Analytics.Model.Entities.IContactEmailAddresses>("Emails");
-                    emails.Preferred = "work";
-                    emails.Entries.Create(emails.Preferred);
-                    emails.Entries[emails.Preferred].SmtpAddress = email;
-                    emails.Entries[emails.Preferred].BounceCount = 0;
-
-                    var personalInfo = Tracker.Current.Contact.GetFacet<Sitecore.Analytics.Model.Entities.IContactPersonalInfo>("Personal");
-                    personalInfo.FirstName = addressRendering.BillingAddress.FirstName;
-                    personalInfo.Surname = addressRendering.BillingAddress.LastName;
-                }
-            }
-
+            
             _transactionLibraryInternal.ExecuteBasketPipeline();
 
             return Json(new {ShippingUrl = "/shipping"});
