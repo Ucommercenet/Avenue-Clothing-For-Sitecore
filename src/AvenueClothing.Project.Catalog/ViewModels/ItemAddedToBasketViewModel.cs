918eb321ec2131a87f3bed95364e986f771315e1
Mads Due <devnull@localhost>
2017-01-13 15:59:19 +0100

    Commerce Connect. WIP on Banner with message when new item added to basket

---
+++ src/AvenueClothing.Project.Catalog/ViewModels/ItemAddedToBasketViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Web;
+
+namespace AvenueClothing.Project.Catalog.ViewModels
+{
+    public class ItemAddedToBasketViewModel
+    {
+        public HtmlString Message { get; set; }
+    }
+}
