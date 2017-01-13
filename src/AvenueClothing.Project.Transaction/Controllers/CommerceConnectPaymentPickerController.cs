e870df4fdff92eda0be45988eb03a8a2b6c6aae6
jesper nielsen <jesper.nielsen@ucommerce.dk>
2017-01-13 11:22:39 +0100

    Added new Commerce Connect controller file and a new view. Both are just copies of the original files for now.

---
+++ src/AvenueClothing.Project.Transaction/Controllers/CommerceConnectPaymentPickerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using AvenueClothing.Foundation.MvcExtensions;
+using AvenueClothing.Project.Transaction.ViewModels;
+using UCommerce;
+using UCommerce.Transactions;
+
+namespace AvenueClothing.Project.Transaction.Controllers
+{
+    public class CommerceConnectPaymentPickerController : BaseController
+    {
+        private readonly TransactionLibraryInternal _transactionLibraryInternal;
+
+        public CommerceConnectPaymentPickerController(TransactionLibraryInternal transactionLibraryInternal)
+        {
+            _transactionLibraryInternal = transactionLibraryInternal;
+        }
+
+        public ActionResult Rendering()
+        {
+            var paymentPickerViewModel = new PaymentPickerViewModel();
+
+            var basket = _transactionLibraryInternal.GetBasket(false).PurchaseOrder;
+            var shippingCountry = basket.GetShippingAddress(Constants.DefaultShipmentAddressName).Country;
+
+            paymentPickerViewModel.ShippingCountry = shippingCountry.Name;
+
+            var availablePaymentMethods = _transactionLibraryInternal.GetPaymentMethods(shippingCountry);
+
+            var existingPayment = basket.Payments.FirstOrDefault();
+            paymentPickerViewModel.SelectedPaymentMethodId = existingPayment != null
+                ? existingPayment.PaymentMethod.PaymentMethodId
+                : -1;
+
+            foreach (var availablePaymentMethod in availablePaymentMethods)
+            {
+                var option = new SelectListItem();
+                decimal feePercent = availablePaymentMethod.FeePercent;
+                var fee = availablePaymentMethod.GetFeeForCurrency(basket.BillingCurrency);
+                var formattedFee = new Money((fee == null ? 0 : fee.Fee), basket.BillingCurrency);
+
+                option.Text = String.Format(" {0} ({1} + {2}%)", availablePaymentMethod.Name, formattedFee,
+                    feePercent.ToString("0.00"));
+                option.Value = availablePaymentMethod.PaymentMethodId.ToString();
+                option.Selected = availablePaymentMethod.PaymentMethodId == paymentPickerViewModel.SelectedPaymentMethodId;
+
+                paymentPickerViewModel.AvailablePaymentMethods.Add(option);
+            }
+
+            return View(paymentPickerViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult CreatePayment(PaymentPickerViewModel createPaymentViewModel)
+        {
+            _transactionLibraryInternal.CreatePayment(createPaymentViewModel.SelectedPaymentMethodId, -1m, false, true);
+            _transactionLibraryInternal.ExecuteBasketPipeline();
+
+            return Redirect("/preview");
+        }
+    }
+}
