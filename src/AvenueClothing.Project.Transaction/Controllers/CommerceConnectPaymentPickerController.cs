bf0eac5c70437564eb9744589227543c856417bb
jesper nielsen <jesper.nielsen@ucommerce.dk>
2017-01-17 16:15:06 +0100

    Added the CC functionality  for adding payments to an order.

--- src/AvenueClothing.Project.Transaction/Controllers/CommerceConnectPaymentPickerController.cs
+++ src/AvenueClothing.Project.Transaction/Controllers/CommerceConnectPaymentPickerController.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Web.Mvc;
 using AvenueClothing.Foundation.MvcExtensions;
 using AvenueClothing.Project.Transaction.ViewModels;
+using Sitecore;
+using Sitecore.Commerce.Contacts;
+using Sitecore.Commerce.Entities.Carts;
+using Sitecore.Commerce.Entities.Payments;
+using Sitecore.Commerce.Entities.Shipping;
+using Sitecore.Commerce.Services.Carts;
+using Sitecore.Commerce.Services.Payments;
+using Sitecore.Commerce.Services.Shipping;
+using Sitecore.Configuration;
 using UCommerce;
 using UCommerce.Transactions;
+using Constants = UCommerce.Constants;
 
 namespace AvenueClothing.Project.Transaction.Controllers
 {
@@ -21,29 +33,36 @@ namespace AvenueClothing.Project.Transaction.Controllers
         {
             var paymentPickerViewModel = new PaymentPickerViewModel();
 
-            var basket = _transactionLibraryInternal.GetBasket(false).PurchaseOrder;
-            var shippingCountry = basket.GetShippingAddress(Constants.DefaultShipmentAddressName).Country;
 
-            paymentPickerViewModel.ShippingCountry = shippingCountry.Name;
+            var cart = GetCart();
+            var paymentOption = GetPaymentOptions(cart.ShopName).FirstOrDefault();
+            var paymentMethods = GetPaymentMethods(paymentOption, cart);
+            var paymentMethodPrices = GetPaymentMethodPrices(paymentMethods, cart);
 
-            var availablePaymentMethods = _transactionLibraryInternal.GetPaymentMethods(shippingCountry);
 
-            var existingPayment = basket.Payments.FirstOrDefault();
-            paymentPickerViewModel.SelectedPaymentMethodId = existingPayment != null
-                ? existingPayment.PaymentMethod.PaymentMethodId
-                : -1;
+            var payment = cart.Payment.FirstOrDefault();
 
-            foreach (var availablePaymentMethod in availablePaymentMethods)
+            if (payment != null)
+            {
+                int paymentMethodId;
+                if (int.TryParse(payment.PaymentMethodID, out paymentMethodId))
+                {
+                    paymentPickerViewModel.SelectedPaymentMethodId = paymentMethodId;
+                }
+            }
+
+            foreach (var paymentMethod in paymentMethods)
             {
                 var option = new SelectListItem();
-                decimal feePercent = availablePaymentMethod.FeePercent;
-                var fee = availablePaymentMethod.GetFeeForCurrency(basket.BillingCurrency);
-                var formattedFee = new Money((fee == null ? 0 : fee.Fee), basket.BillingCurrency);
+                var paymentPrice = paymentMethodPrices.First(x => x.MethodId == paymentMethod.ExternalId);
+                decimal feePercent = (decimal)paymentPrice.GetPropertyValue("FeePercent");
+                var fee = paymentPrice.Amount;
+                var formattedFee = string.Format("{0} {1}", fee, paymentPrice.CurrencyCode);
 
-                option.Text = String.Format(" {0} ({1} + {2}%)", availablePaymentMethod.Name, formattedFee,
+                option.Text = String.Format(" {0} ({1} + {2}%)", paymentMethod.Name, formattedFee,
                     feePercent.ToString("0.00"));
-                option.Value = availablePaymentMethod.PaymentMethodId.ToString();
-                option.Selected = availablePaymentMethod.PaymentMethodId == paymentPickerViewModel.SelectedPaymentMethodId;
+                option.Value = paymentMethod.ExternalId;
+                option.Selected = paymentMethod.ExternalId == paymentPickerViewModel.SelectedPaymentMethodId.ToString();
 
                 paymentPickerViewModel.AvailablePaymentMethods.Add(option);
             }
@@ -54,10 +73,73 @@ namespace AvenueClothing.Project.Transaction.Controllers
         [HttpPost]
         public ActionResult CreatePayment(PaymentPickerViewModel createPaymentViewModel)
         {
-            _transactionLibraryInternal.CreatePayment(createPaymentViewModel.SelectedPaymentMethodId, -1m, false, true);
-            _transactionLibraryInternal.ExecuteBasketPipeline();
+            var cartService = new CartServiceProvider();
+            var cart = GetCart();
+
+            var paymentList = new List<PaymentInfo>
+            {
+                new PaymentInfo()
+                {
+                    PaymentMethodID = createPaymentViewModel.SelectedPaymentMethodId.ToString(),
+                }
+            };
+
+            var addRequest = new AddPaymentInfoRequest(cart, paymentList);
+            var addResult = cartService.AddPaymentInfo(addRequest);
 
             return Redirect("/preview");
+        }
+
+        private Cart GetCart()
+        {
+            var cartServiceProvider = new CartServiceProvider();
+
+            var contactFactory = new ContactFactory();
+            string userId = contactFactory.GetContact();
+
+            var createCartRequest = new CreateOrResumeCartRequest(Context.GetSiteName(), userId);
+
+            return cartServiceProvider.CreateOrResumeCart(createCartRequest).Cart;
+        }
+
+        private ReadOnlyCollection<PaymentOption> GetPaymentOptions(string shopName)
+        {
+            var paymentService = new PaymentServiceProvider();
+            var request = new GetPaymentOptionsRequest(shopName);
+
+            return paymentService.GetPaymentOptions(request).PaymentOptions;
+        }
+
+        private ReadOnlyCollection<PaymentMethod> GetPaymentMethods(PaymentOption paymentOption, Cart cart)
+        {
+            var paymentService = new PaymentServiceProvider();
+            var request = new GetPaymentMethodsRequest(paymentOption, cart.ShopName);
+
+            var shippingParty = cart.Parties.FirstOrDefault(x => (string)x.Properties["Name"] == Constants.DefaultShipmentAddressName);
+            if (shippingParty != null)
+            {
+                request.Properties["Country"] = shippingParty.Country;
+            }
+
+            return paymentService.GetPaymentMethods(request).PaymentMethods;
+        }
+
+        private ReadOnlyCollection<PaymentPrice> GetPaymentMethodPrices(ReadOnlyCollection<PaymentMethod> paymentMethods, Cart cart)
+        {
+            var paymentLookupList = new List<PaymentLookup>();
+
+            foreach (var paymentMethod in paymentMethods)
+            {
+                paymentLookupList.Add(new PaymentLookup()
+                {
+                    MethodId = paymentMethod.PaymentOptionId
+                });
+            }
+
+            var paymentService = new PaymentServiceProvider();
+            var request = new GetPricesForPaymentsRequest(cart.ShopName, paymentLookupList, cart);
+
+            return paymentService.GetPricesForPayments(request).PaymentPrices;
         }
     }
 }
