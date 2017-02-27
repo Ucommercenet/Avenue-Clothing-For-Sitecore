f700784a90a52885d6b071f4ad2ca73a4940de0d
Alice Puricica <alice.puricica@ucommerce.dk>
2017-02-27 14:29:50 +0100

    Added js for search bar in tablet/mobile and updated media queries

---
+++ src/AvenueClothing.Project.Catalog/Scripts/jsSearchbar.js
@@ -0,0 +1,26 @@
+﻿define('jsSearchbar', ['jquery', 'jsConfig'], function ($, config) {
+    'use strict';
+
+    /** Behaviour on Tablet/Mobile screen**/
+    var classSelectorMobile = ".search-on-small-screen.hide-on-large-screen";
+    var searchForm = ".search-form";
+    var navbrand = ".navbar-brand";
+    var clicked = false;
+
+
+    var jsSearchbar = {};
+
+    jsSearchbar.init = function () {
+        $(classSelectorMobile).click(function () {
+            if (!clicked) {
+                $(navbrand).innerHTML = "";
+                $(searchForm).css("display", "inline-block");
+                clicked = true;
+            } else {
+                $(searchForm).css("display", "none");
+                clicked = false;
+            }
+        });
+    };
+    return jsSearchbar;
+});
